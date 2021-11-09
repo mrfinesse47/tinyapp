da3383f6dfb40a744bf5696a613d9f3c8498c604
Kevin Mason <krmason2020@gmail.com>
2021-11-09 21:11:44 +0000

    created delete route POST http://localhost:8080/urls/9sm5xK/delete

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const { v4: uuidv4 } = require("uuid"); //used to generate random string for generateRandomString()
 
 const PORT = 8080;
@@ -7,6 +8,7 @@ const PORT = 8080;
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -60,6 +62,19 @@ app.post("/urls", (req, res) => {
 
   // res.send("Ok"); // Respond with 'Ok' (we will replace this)
   res.redirect(`/urls/${key}`);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  // console.log(req.body); // Log the POST request body to the console
+  // const key = generateRandomString();
+  // urlDatabase[key] = req.body.longURL;
+  //console.log(urlDatabase);
+  console.log("here");
+  console.log(req.params.shortURL);
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  // res.redirect(`/urls/${key}`);
 });
 
 app.listen(PORT, () => {
